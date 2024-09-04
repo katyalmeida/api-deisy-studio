40692b0a64bb6b204eef03d256741d3332a22827
katyalmeida <almeidakatiane29@gmail.com>
2024-09-03 23:14:04 -0300

    feat: add multer for file uploads

---
+++ src/config/multer.js
@@ -0,0 +1,16 @@
+import multer from 'multer';
+import { v4 } from 'uuid';
+import { extname, resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+  storage: multer.diskStorage({
+    destination: resolve(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, callback) => {
+      callback(null, v4() + extname(file.originalname));
+    },
+  }),
+};
