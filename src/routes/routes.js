e4d595c8add34db4295728f7955499dad445fed5
katyalmeida <almeidakatiane29@gmail.com>
2024-09-03 23:24:02 -0300

    feat: add new routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,12 @@ import { Router } from 'express';
 
 import UserController from '../app/controllers/UserController.js';
 import LoginController from '../app/controllers/LoginController.js';
+import SpecialtyController from '../app/controllers/SpecialtyController.js';
+import ServicesController from '../app/controllers/ServicesController.js';
 import authMiddleware from '../app/middlewares/auth.js';
+import multer from 'multer';
+import multerConfig from '../config/multer.js';
+const upload = multer(multerConfig);
 const routes = new Router();
 
 routes.post('/login', LoginController.store);
@@ -12,5 +17,13 @@ routes.use(authMiddleware);
 routes.get('/users', UserController.index);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
+
+routes.get('/specialty', SpecialtyController.index);
+routes.post('/specialty', upload.single('file'), SpecialtyController.store);
+routes.put('/specialty/:id', upload.single('file'), SpecialtyController.update);
+
+routes.get('/services', ServicesController.index);
+routes.post('/services', upload.single('file'), ServicesController.store);
+routes.put('/services/:id', upload.single('file'), ServicesController.update);
 
 export default routes;
